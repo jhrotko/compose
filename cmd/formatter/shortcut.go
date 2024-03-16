c2b243c33b111b2b0d24aabb1b690e9ca11a539a
Joana Hrotko <joana.hrotko@docker.com>
2024-03-16 16:59:02 +0000

    Implement error design
    Signed-off-by: Joana Hrotko <joana.hrotko@docker.com>

--- cmd/formatter/shortcut.go
+++ cmd/formatter/shortcut.go
@@ -22,8 +22,8 @@ import (
 var DISPLAY_ERROR_TIME = 10
 
 type KeyboardError struct {
-	err      error
-	errStart time.Time
+	err       error
+	timeStart time.Time
 }
 type KeyboardWatch struct {
 	Watcher  watch.Notify
@@ -55,7 +55,6 @@ type LogKeyboard struct {
 
 var KeyboardManager *LogKeyboard
 var eg multierror.Group
-var errorColor = "\x1b[1;33m"
 
 func NewKeyboardManager(isDockerDesktopActive, isWatchConfigured bool, sc chan<- os.Signal, watchFn func(ctx context.Context, project *types.Project, services []string, options api.WatchOptions) error) {
 	km := LogKeyboard{}
@@ -75,15 +74,15 @@ func NewKeyboardManager(isDockerDesktopActive, isWatchConfigured bool, sc chan<-
 }
 
 func (lk *LogKeyboard) PrintKeyboardInfo(print func()) {
-	fmt.Print("\033[?25l")        // hide cursor
-	defer fmt.Printf("\033[?25h") // show cursor
+	fmt.Print("\033[?25l") // hide cursor
+	// defer fmt.Printf("\033[?25h") // show cursor
 
 	lk.clearInfo()
 	print()
 
 	switch lk.logLevel {
 	case INFO:
-		lk.createBuffer(0)
+		lk.createBuffer(1)
 		lk.printInfo()
 	case DEBUG:
 		lk.createBuffer(3)
@@ -93,8 +92,9 @@ func (lk *LogKeyboard) PrintKeyboardInfo(print func()) {
 }
 
 func (lk *LogKeyboard) Error(prefix string, err error) {
-	lk.ErrorHandle.errStart = time.Now()
-	lk.ErrorHandle.err = fmt.Errorf("[%s]  %s", prefix, err.Error())
+	lk.ErrorHandle.timeStart = time.Now()
+	errorString := fmt.Sprintf("\033[1;36m%s →  \033[0m%s", prefix, err.Error())
+	lk.ErrorHandle.err = fmt.Errorf(errorString)
 }
 
 func allocateSpace(lines int) {
@@ -125,7 +125,7 @@ func (lk *LogKeyboard) createBuffer(lines int) {
 }
 
 func (ke *KeyboardError) shoudlDisplay() bool {
-	return ke.err != nil && int(time.Since(ke.errStart).Seconds()) < DISPLAY_ERROR_TIME
+	return ke.err != nil && int(time.Since(ke.timeStart).Seconds()) < DISPLAY_ERROR_TIME
 }
 
 func isOverflow(s string) bool {
@@ -139,8 +139,8 @@ func linesOffset(s string) int {
 func (lk *LogKeyboard) printError(height int, info string) {
 	if lk.ErrorHandle.shoudlDisplay() {
 		errMessage := lk.ErrorHandle.err.Error()
-		fmt.Printf("\033[%d;0H\033[2K", height-linesOffset(info)-linesOffset(errMessage)-1) // Move to before last line
-		fmt.Printf(errorColor + errMessage + "\033[0m")
+		fmt.Printf("\033[%d;0H\033[2K", height-linesOffset(info)-linesOffset(errMessage)-1)
+		fmt.Printf(errMessage + "\033[0m")
 	}
 }
 
@@ -185,7 +185,7 @@ func (lk *LogKeyboard) infoMessage() string {
 	if lk.Watch.Watching {
 		isEnabled = " Disable"
 	}
-	watchInfo = watchInfo + shortcutKeyColor("V") + navColor(isEnabled+" Watch")
+	watchInfo = watchInfo + shortcutKeyColor("W") + navColor(isEnabled+" Watch")
 	return options + openDDInfo + watchInfo
 }
 
@@ -235,7 +235,7 @@ func (lk *LogKeyboard) openDockerDesktop(project *types.Project) {
 
 func (lk *LogKeyboard) StartWatch(ctx context.Context, project *types.Project, options api.UpOptions) {
 	if !lk.IsWatchConfigured {
-		lk.Error("Watch", fmt.Errorf("Watch is not yet configured. Learn more: https://docs.docker.com/compose/file-watch/"))
+		lk.Error("Watch", fmt.Errorf("Watch is not yet configured. Learn more: \033[36mhttps://docs.docker.com/compose/file-watch/\033[0m"))
 		return
 	}
 	lk.switchWatching()
@@ -355,7 +355,7 @@ func (lk *LogKeyboard) HandleKeyEvents(event keyboard.KeyEvent, ctx context.Cont
 					return nil
 				})(ctx)
 		}()
-
+		fmt.Printf("\033[?25h") // show cursor
 		// will notify main thread to kill and will handle gracefully
 		lk.SignalChannel <- syscall.SIGINT
 	case keyboard.KeyEnter:
